e394b78ea4e1745e29013b6fa1a820c5762489b7
Nkapj <jessica.nzi@outlook.com>
2024-09-05 17:06:32 +0200

    Ajout des fichiers index.html et app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var a = "3";
+var b = "8";
+
+/***********Do not change the code above 👆*******/
+//Write your code here : 
+var tempo = a;
+a = b;
+b = tempo ;
+
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
