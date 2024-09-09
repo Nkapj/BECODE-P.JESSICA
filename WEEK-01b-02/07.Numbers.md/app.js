65954d190596476b9bb5982c705131e61604737d
Nkapj <jessica.nzi@outlook.com>
2024-09-09 13:58:19 +0200

    07.Numbers.md

---
+++ WEEK-01b-02/07.Numbers.md/app.js
@@ -0,0 +1,30 @@
+//Exercise :
+/*
+Go look how can we get a random number in JS !
+You will have to use another Math method : random.
+Get to really understand how the random() method works. Watch this (only the first 1min50)
+Generate a random integer between 0 and 10
+Generate a random float between 1 and 5 */
+
+let x = Math.floor(Math.random()* 11);
+
+console.log(x);
+
+
+let y = Math.random()*4 +1;
+console.log(y);
+
+// -------------------------------
+let myNum = 0;
+
+myNum += 4;
+
+myNum -= 2;
+
+myNum /= 2;
+
+myNum *= 10;
+
+console.log(myNum);
+
+//--------------------------------
