46d645234e0f3124febfb303b8719f5cfdd9e587
Nkapj <jessica.nzi@outlook.com>
2024-09-09 09:32:50 +0200

    06b.Practice.md/

--- WEEK-01b-02/06b.Practice.md/app.js
+++ WEEK-01b-02/06b.Practice.md/app.js
@@ -8,3 +8,16 @@ const whisper= message.trim().toLowerCase();
 
 console.log(whisper);
 
+
+let zord = "Dumbledore";
+let final = zord[zord.length-1]
+console.log(final);
+
+
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE
+const facialHair = word.slice(5).replace('o','e');
+
+console.log(facialHair);
