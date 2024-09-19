695a0e1be34975d055ba0dd530170a054680ffda
Nkapj <jessica.nzi@outlook.com>
2024-09-19 09:36:17 +0200

    01.exo.md

---
+++ WEEK-03-04/01.ALGOS-serie1/01.exo.md/app.js
@@ -0,0 +1,26 @@
+function year(age) {
+    const result = (age * 365);
+    return result;
+} 
+console.log(year(32));
+console.log(year(30));
+console.log(year(55));
+console.log(year(38));
+console.log(year(67));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
