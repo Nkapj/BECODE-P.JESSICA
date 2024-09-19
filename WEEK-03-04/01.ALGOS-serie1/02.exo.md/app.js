13053ada720c9864a0ee2f8265f8d552921d7e2b
Nkapj <jessica.nzi@outlook.com>
2024-09-19 11:06:14 +0200

    algos

---
+++ WEEK-03-04/01.ALGOS-serie1/02.exo.md/app.js
@@ -0,0 +1,12 @@
+function addUp(num) {
+    let total = 0;
+for ( let i = 1; i <= num; i++){
+    total += i
+}
+return total;
+}
+
+console.log(addUp(4));
+console.log(addUp(10));
+console.log(addUp(20));
+console.log(addUp(15));
