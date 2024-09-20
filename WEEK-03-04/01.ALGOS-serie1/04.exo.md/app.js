0a5266bf2be37db47445f9c5b807c6ff7c1ef072
Nkapj <jessica.nzi@outlook.com>
2024-09-20 11:57:20 +0200

    algos

---
+++ WEEK-03-04/01.ALGOS-serie1/04.exo.md/app.js
@@ -0,0 +1,10 @@
+function mot(word){
+    let minus = "";
+ for (let i= 0; i < word.length; i++){
+ if (word[i] === word[i].toLowerCase()) {
+    minus += word[i];
+ } 
+ } return minus;
+
+}
+ console.log(mot("JDPQHSJiihdfugafahOGOHohashohcbD"));
