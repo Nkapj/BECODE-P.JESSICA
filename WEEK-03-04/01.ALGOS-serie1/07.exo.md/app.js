f449a23aa2f38504d0d10b379e23a75e11cb256f
Nkapj <jessica.nzi@outlook.com>
2024-09-23 13:14:39 +0200

    algos

---
+++ WEEK-03-04/01.ALGOS-serie1/07.exo.md/app.js
@@ -0,0 +1,10 @@
+
+function calcul(probability,prize,pay) {
+    if (probability*prize - pay > 0 ){
+        return true;
+    }else {
+        return false;
+    } 
+
+}
+console.log(calcul(25,10,5));
