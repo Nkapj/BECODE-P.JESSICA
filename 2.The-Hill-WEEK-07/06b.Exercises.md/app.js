06527da9daac63b2d24f3fd32d5e46656e6ad3ae
Nkapj <jessica.nzi@outlook.com>
2024-10-17 19:03:42 +0200

    DOM

---
+++ 2.The-Hill-WEEK-07/06b.Exercises.md/app.js
@@ -0,0 +1,29 @@
+const bold = document.querySelector("b");
+
+
+bold.style.color="red";
+
+document.querySelector("body").style.backgroundColor="beige"
+
+const part2 = document.querySelector(".part-2");
+part2.style.backgroundColor= "darkblue";
+part2.style.color = "white";
+bold.style.color = "red";
+
+let myBtn=document.querySelector(".myBtn");
+
+myBtn.style.color="pink";
+
+let liste = document.querySelectorAll("li");
+liste.forEach(function (li) {
+    li.style.color ="orange";
+});
+
+
+let tete = document.querySelectorAll('li a');
+tete.forEach(function(a){
+    a.style.color = "green"
+});
+
+document.querySelector("h1").style.textTransform = "uppercase";
+
